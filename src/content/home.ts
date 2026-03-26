d7379d53f399472d38794e8bbcc731e0ab2a6106
Asher Pope <asher.pope@intrinsiclabs.co>
2026-03-26 03:10:00 -0400

    update about page

--- src/content/home.ts
+++ src/content/home.ts
@@ -5,4 +5,4 @@ export type {
 } from "@/content/case-studies/types";
 
 export const aboutTeaser =
-  'I\'m a self taught software engineer. I firmly believe you can learn anything if you just sit down and work at it. I got tired of wishing apps would "just do this one thing", so I learned to code (pre-ChatGPT). I study the top players in a space, learn why they do things the way they do, then go and build cool stuff that can hold its own.';
+  'I\'m a self taught software engineer. I firmly believe you can learn anything if you just sit down and work at it. I got tired of wishing apps would "just do this one thing", so I learned to code (the hard way, pre-ChatGPT). I study the top players in a space, learn why they do things the way they do, then go and build cool stuff that can hold its own.';
